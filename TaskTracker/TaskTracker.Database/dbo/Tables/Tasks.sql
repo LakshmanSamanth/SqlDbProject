c203704aa8899c976bd7a11012a0519fc53b32ef
fhilton <mr.hilton@gmail.com>
2015-03-18 15:24:34 -0400

    Removed Start and End Date to get ready for demo

--- TaskTracker/TaskTracker.Database/dbo/Tables/Tasks.sql
+++ TaskTracker/TaskTracker.Database/dbo/Tables/Tasks.sql
@@ -4,8 +4,6 @@
     [Description] NVARCHAR (MAX) NULL,
     [Complete]    BIT            NOT NULL,
     [ProjectId]   INT            NOT NULL,
-    [StartDate] DATETIME NULL, 
-    [EndDate] DATETIME NULL, 
     CONSTRAINT [PK_dbo.Tasks] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_dbo.Tasks_dbo.Projects_ProjectId] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Projects] ([Id]) ON DELETE CASCADE
 );
