18e909eb6ad780b12f5248ac19ee0e572907ec44
fhilton <mr.hilton@gmail.com>
2015-09-18 07:26:11 -0400

    Added CRM Db to demonstrate references

---
+++ TaskTracker/CRM/dbo/Tables/Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT          NOT NULL,
+    [UserName]  VARCHAR (50) NULL,
+    [FirstName] VARCHAR (50) NULL,
+    [LastName]  VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
