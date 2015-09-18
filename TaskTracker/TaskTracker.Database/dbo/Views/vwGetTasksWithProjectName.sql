f4351594938d82de0f34fffc17acdfe6309beff2
fhilton <mr.hilton@gmail.com>
2015-09-18 13:07:58 -0400

    Successful Build

--- TaskTracker/TaskTracker.Database/dbo/Views/vwGetTasksWithProjectName.sql
+++ TaskTracker/TaskTracker.Database/dbo/Views/vwGetTasksWithProjectName.sql
@@ -3,7 +3,7 @@
 	Select 
 		t.Name as TaskName, 
 		p.Name as ProjectName
-	FROM TaskTracker.dbo.Tasks t
+	FROM dbo.Tasks t
 	inner join dbo.Projects p
 		on t.ProjectId = p.Id
 	left join [CRM].dbo.Users u
