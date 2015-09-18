f988bd8fa694630042a75e83631e895422aed556
fhilton <mr.hilton@gmail.com>
2015-09-18 13:16:37 -0400

    Added Project Start Date

--- TaskTracker/TaskTracker.Database/dbo/Tables/Projects.sql
+++ TaskTracker/TaskTracker.Database/dbo/Tables/Projects.sql
@@ -3,6 +3,7 @@
     [Name]          NVARCHAR (MAX) NULL,
     [Description]   NVARCHAR (MAX) NULL,
     [ManagerUserId] NVARCHAR (128) NULL,
+    [ProjectStartDate] DATE NULL, 
     CONSTRAINT [PK_dbo.Projects] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Projects_AspNetUsers] FOREIGN KEY ([ManagerUserId]) REFERENCES [dbo].[AspNetUsers] ([Id])
 );
